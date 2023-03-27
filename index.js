c27d76c8a69dee4e57e2afc19146212e9b95b1de
odaney baker <bakerobtb@gmail.com>
2023-03-26 21:12:22 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+let currentUser = "John";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+console.assert(welcomeMessage.includes("Welcome to Flatbook") && welcomeMessage.includes(currentUser) && welcomeMessage.endsWith("!"));
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+console.assert(excitedWelcomeMessage.includes("WELCOME TO FLATBOOK") && excitedWelcomeMessage.includes(currentUser) && excitedWelcomeMessage.endsWith("!"));
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+console.assert(shortGreeting.includes("Welcome") && shortGreeting.includes(currentUser.charAt(0)) && shortGreeting.endsWith("!"));
+ 
+
